6c1594db4bf9e60edd5c19d15058c4487318a99c
Max Neuvians <max@neuvians.net>
2020-06-02 19:38:00 -0400

    Missed variable

--- config/terraform/aws/ecs.tf
+++ config/terraform/aws/ecs.tf
@@ -101,7 +101,7 @@ data "template_file" "covidshield_key_submission_task" {
   template = file("task-definitions/covidshield_key_submission.json")
 
   vars = {
-    image                 = "covidshield/key-submission:${coalesce(var.github_sha, data.github_branch.server.sha)}"
+    image                 = "covidshield/key-submission:${coalesce(var.github_sha, "latest")}"
     awslogs-group         = aws_cloudwatch_log_group.covidshield.name
     awslogs-region        = var.region
     awslogs-stream-prefix = "ecs-${var.ecs_key_submission_name}"
