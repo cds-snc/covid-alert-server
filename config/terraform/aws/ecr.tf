b2c456cc6fac0b003944e36ff849b50b4aa549ed
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-06-03 09:59:56 -0400

    add basic ecr config

---
+++ config/terraform/aws/ecr.tf
@@ -0,0 +1,32 @@
+resource "aws_ecr_repository" "repository" {
+  name                 = "covid-server"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "policy" {
+  repository = "${aws_ecr_repository.repository.name}"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
