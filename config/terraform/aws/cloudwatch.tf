ec50a3140f95967596dfb0e5380212c395827d8c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-06-18 15:59:52 -0400

    add cpu utilization alarms

--- config/terraform/aws/cloudwatch.tf
+++ config/terraform/aws/cloudwatch.tf
@@ -5,3 +5,41 @@ resource "aws_cloudwatch_log_group" "covidshield" {
     (var.billing_tag_key) = var.billing_tag_value
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "retrieval_cpu_utilization_high" {
+  alarm_name          = "retrieval-cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs cpu utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_retrieval.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "submission_cpu_utilization_high" {
+  alarm_name          = "submission-cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs cpu utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_submission.name
+  }
+}
