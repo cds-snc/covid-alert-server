7667a7381f09893db7575be93ab9190722427c85
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-06-22 10:16:05 -0400

    Add some CloudWatch alarms (#45)
    * add cpu utilization alarms
    
    * add memory utilization alerts

--- config/terraform/aws/cloudwatch.tf
+++ config/terraform/aws/cloudwatch.tf
@@ -5,3 +5,79 @@ resource "aws_cloudwatch_log_group" "covidshield" {
     (var.billing_tag_key) = var.billing_tag_value
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "retrieval_cpu_utilization_high" {
+  alarm_name          = "retrieval-cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs cpu utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_retrieval.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "submission_cpu_utilization_high" {
+  alarm_name          = "submission-cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs cpu utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_submission.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "retrieval_memory_utilization_high" {
+  alarm_name          = "retrieval-memory-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs memory utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_retrieval.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "submission_memory_utilization_high" {
+  alarm_name          = "submission-memory-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "MemoryUtilization"
+  namespace           = "AWS/ECS"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "This metric monitors ecs memory utilization"
+
+  alarm_actions = [aws_sns_topic.alert_warning.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.covidshield.name
+    ServiceName = aws_ecs_service.covidshield_key_submission.name
+  }
+}
