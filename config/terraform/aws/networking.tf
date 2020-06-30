7d07649fc93879951f3562329b344d5a7ca80c4f
Max Neuvians <max@neuvians.net>
2020-06-30 14:12:57 -0400

    Add some block rules to default NACL (#105)

--- config/terraform/aws/networking.tf
+++ config/terraform/aws/networking.tf
@@ -246,3 +246,51 @@ resource "aws_security_group" "covidshield_egress_anywhere" {
     (var.billing_tag_key) = var.billing_tag_value
   }
 }
+
+###
+# AWS Network ACL
+###
+
+resource "aws_default_network_acl" "covidshield" {
+  default_network_acl_id = aws_vpc.covidshield.default_network_acl_id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 3389
+    to_port    = 3389
+  }
+
+  ingress {
+    protocol   = -1
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+}
