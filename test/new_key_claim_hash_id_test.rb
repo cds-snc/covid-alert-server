4f8dbe2b4d967323646952b67c9133ce167323bb
Saini <Simmi.Saini@ontario.ca>
2020-07-07 15:48:37 -0400

    updated the error codes for /new-key-claim endpoint that generates 8 digit code

--- test/new_key_claim_hash_id_test.rb
+++ test/new_key_claim_hash_id_test.rb
@@ -17,7 +17,7 @@ class NewKeyClaimhashIDTest < MiniTest::Test
 
     %w[get patch delete put].each do |meth|
       resp = @sub_conn.send(meth, "/new-key-claim/#{hash_id}")
-      assert_response(resp, 405, 'text/plain; charset=utf-8', body: "method not allowed\n")
+      assert_response(resp, 401, 'text/plain; charset=utf-8', body: "unauthorized\n")
     end
 
     resp = @sub_conn.post do |req|
@@ -52,7 +52,7 @@ class NewKeyClaimhashIDTest < MiniTest::Test
       req.url("/new-key-claim/#{hash_id}")
       req.headers['Authorization'] = 'Bearer second-very-long-token'
     end
-    assert_response(resp, 401, 'text/plain; charset=utf-8', body: "unauthorized\n")
+    assert_response(resp, 403, 'text/plain; charset=utf-8', body: "forbidden\n")
 
   end
 end
