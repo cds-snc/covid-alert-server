1aad55ad332cc56b103d8856c5c4fb41396c1ab6
Dan Woytowich <dan.woytowich@ontario.ca>
2020-07-07 15:56:02 -0400

    Changed to https-only

--- config/terraform/aws/cloudfront.tf
+++ config/terraform/aws/cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_distribution" "key_retrieval_distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "http-only"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
