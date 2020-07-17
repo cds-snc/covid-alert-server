f397b2006cad8fdefb68fd8cfe9c283678ed0bfd
Max Neuvians <max@neuvians.net>
2020-07-17 11:31:05 -0400

    Add WAF rules for Key retrieval ALB and CDN

--- config/terraform/aws/waf.tf
+++ config/terraform/aws/waf.tf
@@ -325,12 +325,44 @@ resource "aws_wafv2_web_acl" "key_submission" {
   }
 }
 
+resource "aws_wafv2_web_acl" "key_retrieval" {
+  name  = "key_retrieval"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+}
+
+resource "aws_wafv2_web_acl" "key_retrieval_cdn" {
+  provider = aws.us-east-1
+
+  name  = "key_retrieval_cdn"
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+}
+
 ###
 # AWS WAF - Resource Assocation
 ###
 resource "aws_wafv2_web_acl_association" "key_submission_assocation" {
   resource_arn = aws_lb.covidshield_key_submission.arn
   web_acl_arn  = aws_wafv2_web_acl.key_submission.arn
+}
+
+resource "aws_wafv2_web_acl_association" "key_retrieval_assocation" {
+  resource_arn = aws_lb.covidshield_key_retrieval.arn
+  web_acl_arn  = aws_wafv2_web_acl.key_retrieval.arn
+}
+
+resource "aws_wafv2_web_acl_association" "key_retrieval_cdn_assocation" {
+  provider = aws.us-east-1
+
+  resource_arn = aws_cloudfront_distribution.key_retrieval_distribution.arn
+  web_acl_arn  = aws_wafv2_web_acl.key_retrieval_cdn.arn
 }
 
 ###
@@ -344,4 +376,14 @@ resource "aws_wafv2_web_acl_logging_configuration" "firehose_waf_logs" {
       name = "authorization"
     }
   }
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "firehose_waf_logs_retrieval" {
+  log_destination_configs = ["${aws_kinesis_firehose_delivery_stream.firehose_waf_logs.arn}"]
+  resource_arn            = aws_wafv2_web_acl.key_retrieval.arn
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "firehose_waf_logs_retrieval_cdn" {
+  log_destination_configs = ["${aws_kinesis_firehose_delivery_stream.firehose_waf_logs.arn}"]
+  resource_arn            = aws_wafv2_web_acl.key_retrieval_cdn.arn
 }
