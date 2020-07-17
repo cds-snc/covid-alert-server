b53c8d61c4af39e8fedc4536d323044232064154
Max Neuvians <max@neuvians.net>
2020-07-17 15:18:35 -0400

    US-EAST kinesis stream

--- config/terraform/aws/waf.tf
+++ config/terraform/aws/waf.tf
@@ -395,3 +395,10 @@ resource "aws_wafv2_web_acl_logging_configuration" "firehose_waf_logs_retrieval"
   log_destination_configs = ["${aws_kinesis_firehose_delivery_stream.firehose_waf_logs.arn}"]
   resource_arn            = aws_wafv2_web_acl.key_retrieval.arn
 }
+
+resource "aws_wafv2_web_acl_logging_configuration" "firehose_waf_logs_retrieval_cdn" {
+  provider = aws.us-east-1
+
+  log_destination_configs = ["${aws_kinesis_firehose_delivery_stream.firehose_waf_logs_us_east.arn}"]
+  resource_arn            = aws_wafv2_web_acl.key_retrieval_cdn.arn
+}
