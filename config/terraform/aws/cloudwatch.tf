54adaf6819c91d7a8bacb26c5fcd1e02c3466712
Max Neuvians <max@neuvians.net>
2020-07-20 10:04:22 -0400

    Set log retention (#188)

--- config/terraform/aws/cloudwatch.tf
+++ config/terraform/aws/cloudwatch.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudwatch_log_group" "covidshield" {
-  name       = var.cloudwatch_log_group_name
-  kms_key_id = aws_kms_key.cw.arn
+  name              = var.cloudwatch_log_group_name
+  kms_key_id        = aws_kms_key.cw.arn
+  retention_in_days = 365
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
