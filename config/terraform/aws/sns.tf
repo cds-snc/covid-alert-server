196bae0eed9753dd5ef7267f2958b3c0707d7c51
Max Neuvians <max@neuvians.net>
2020-07-22 09:17:22 -0400

    fmt

--- config/terraform/aws/sns.tf
+++ config/terraform/aws/sns.tf
@@ -29,7 +29,7 @@ resource "aws_sns_topic" "alert_warning_us_east" {
 
 resource "aws_sns_topic" "alert_critical_us_east" {
   provider = aws.us-east-1
-  
+
   name              = "alert-critical"
   kms_master_key_id = aws_kms_key.cw.arn
 
