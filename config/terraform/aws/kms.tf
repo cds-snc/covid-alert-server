75bf7ba5448a048e24aa4ae9eeb11fcdf8d1811a
Max Neuvians <max@neuvians.net>
2020-07-27 16:00:43 -0400

    Creates KMS in US-East and uses it for SNS

--- config/terraform/aws/kms.tf
+++ config/terraform/aws/kms.tf
@@ -46,3 +46,52 @@ resource "aws_kms_key" "cw" {
 }
 EOF
 }
+
+resource "aws_kms_key" "cw_us_east" {
+  provider          = aws.us-east-1
+
+  description         = "CloudWatch Log Group Key"
+  enable_key_rotation = true
+
+  policy = <<EOF
+{
+  "Version" : "2012-10-17",
+  "Id" : "key-default-1",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.us-east-1.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow_CloudWatch_for_CMK",
+      "Effect": "Allow",
+      "Principal": {
+          "Service":[
+              "cloudwatch.amazonaws.com"
+          ]
+      },
+      "Action": [
+          "kms:Decrypt","kms:GenerateDataKey"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
