5edce77a3a656cafcf421a4f90645fc48982e827
Max Neuvians <max@neuvians.net>
2020-07-28 16:07:50 -0400

    Creates KMS in US-East and uses it for SNS (#221)
    * Creates KMS in US-East and uses it for SNS
    
    * Fmt

--- config/terraform/aws/sns.tf
+++ config/terraform/aws/sns.tf
@@ -20,7 +20,7 @@ resource "aws_sns_topic" "alert_warning_us_east" {
   provider = aws.us-east-1
 
   name              = "alert-warning"
-  kms_master_key_id = aws_kms_key.cw.arn
+  kms_master_key_id = aws_kms_key.cw_us_east.arn
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
@@ -31,7 +31,7 @@ resource "aws_sns_topic" "alert_critical_us_east" {
   provider = aws.us-east-1
 
   name              = "alert-critical"
-  kms_master_key_id = aws_kms_key.cw.arn
+  kms_master_key_id = aws_kms_key.cw_us_east.arn
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
