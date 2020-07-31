43e9ae1de93ad723b29dc7de75ce177c26763b22
Darren Foo <darren.foo@shopify.com>
2020-07-31 12:29:53 -0700

    increase retrieval WAF limit to 10000req/5min/ip

--- config/terraform/aws/waf.tf
+++ config/terraform/aws/waf.tf
@@ -541,7 +541,7 @@ resource "aws_wafv2_web_acl" "key_retrieval_cdn" {
 
     statement {
       rate_based_statement {
-        limit              = 1000
+        limit              = 10000
         aggregate_key_type = "IP"
       }
     }
