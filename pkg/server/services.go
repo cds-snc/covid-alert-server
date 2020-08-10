08ba89e1145448eb6130a0e55613a032a0b85836
Max Neuvians <max@neuvians.net>
2020-08-10 13:34:06 -0400

    Remove urandom sampling (#247)
    * Remove urandom sampling
    
    * Fix imports

--- pkg/server/services.go
+++ pkg/server/services.go
@@ -3,7 +3,6 @@ package server
 import (
 	"encoding/json"
 	"net/http"
-	"os"
 
 	"github.com/Shopify/goose/srvutil"
 	"github.com/gorilla/mux"
@@ -27,9 +26,6 @@ func (s *servicesServlet) RegisterRouting(r *mux.Router) {
 	r.HandleFunc("/ping", s.ping)
 	r.HandleFunc("/present", s.exposurePresence)
 	r.HandleFunc("/version.json", s.version)
-	if os.Getenv("ENV") == "staging" {
-		r.HandleFunc("/urandom.bin", s.urandom)
-	}
 }
 
 func (s *servicesServlet) exposurePresence(w http.ResponseWriter, r *http.Request) {
@@ -64,28 +60,6 @@ func (s *servicesServlet) version(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if _, err := w.Write(js); err != nil {
-		log(ctx, err).Info("error writing response")
-	}
-}
-
-func (s *servicesServlet) urandom(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	f, err := os.Open("/dev/urandom")
-
-	if err != nil {
-		log(ctx, err).Info("error opening urandom")
-	}
-
-	bytes := make([]byte, 1000000)
-	_, err = f.Read(bytes)
-
-	if err != nil {
-		log(ctx, err).Info("error reading data")
-	}
-
-	w.Header().Add("Cache-Control", "application/octet-stream")
-	if _, err := w.Write(bytes); err != nil {
 		log(ctx, err).Info("error writing response")
 	}
 }
