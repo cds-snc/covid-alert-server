a6d9bde8554e729ef2b2d0c3b1b31f2982b0afe3
Max Neuvians <max@neuvians.net>
2020-08-10 14:55:33 -0400

    Part #1: set up buckets and origin

--- config/terraform/aws/s3.tf
+++ config/terraform/aws/s3.tf
@@ -1,6 +1,15 @@
 ###
 # AWS S3 bucket - WAF log target
 ###
+
+resource "aws_s3_bucket" "exposure_config" {
+  bucket = "covid-shield-exposure-config-${var.environment}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.exposure_config_logs.bucket}"
+  }
+}
+
 resource "aws_s3_bucket" "firehose_waf_logs" {
   bucket = "covid-shield-${var.environment}-waf-logs"
   acl    = "private"
@@ -44,6 +53,20 @@ resource "aws_s3_bucket" "cloudfront_logs" {
   #tfsec:ignore:AWS002
 }
 
+resource "aws_s3_bucket" "exposure_config_logs" {
+  bucket = "covid-shield-exposure-config-${var.environment}-logs"
+  acl    = "log-delivery-write"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+  #tfsec:ignore:AWS002
+}
+
 resource "aws_s3_bucket_public_access_block" "firehose_waf_logs" {
   bucket = aws_s3_bucket.firehose_waf_logs.id
 
@@ -61,3 +84,15 @@ resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_public_access_block" "exposure_config_logs" {
+  bucket = aws_s3_bucket.exposure_config_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
