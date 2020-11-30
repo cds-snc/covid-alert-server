2b94afcc03c6fccd98b36c6b9489ba1bddefd737
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-11-30 09:04:18 -0400

    ci(scripts): fix Heroku script as it was failing shellcheck

--- scripts/heroku_run.sh
+++ scripts/heroku_run.sh
@@ -5,17 +5,17 @@ url=$JAWSDB_URL
 no_prot="${url/mysql:\/\//}"
 
 IFS="@"
-read -a strarr <<< "$no_prot"
+read -r -a strarr <<< "$no_prot"
 u_and_p="${strarr[0]}"
 host_and_db="${strarr[1]}"
 
 IFS="/"
-read -a strarr <<< "$host_and_db"
+read -r -a strarr <<< "$host_and_db"
 host_and_port="${strarr[0]}"
 database="${strarr[1]}"
 
 IFS=":"
-read -a strarr <<< "$host_and_port"
+read -r -a strarr <<< "$host_and_port"
 host="${strarr[0]}"
 
 db="${u_and_p}@tcp(${host})/${database}"
