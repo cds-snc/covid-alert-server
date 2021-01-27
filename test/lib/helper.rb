39a4caca1b2224c1db641df49bd557258de3c881
Calvin Rodo <calvin.rodo@cds-snc.ca>
2021-01-27 14:44:14 -0500

    feat: re-enable v2 (#426)

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -71,6 +71,8 @@ module Helper
         transmission_risk_level: transmission_risk_level,
         rolling_period: rolling_period,
         rolling_start_interval_number: rolling_start_interval_number,
+        report_type: "CONFIRMED_TEST",
+        days_since_onset_of_symptoms: 0,
       )
     end
 
